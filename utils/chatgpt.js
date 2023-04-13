558786e497bc3344ab543f65d631c07814454fe3
Nel035 <113762249+Nel035@users.noreply.github.com>
2023-04-13 21:19:08 +0800

    Update chatgpt.js

--- utils/chatgpt.js
+++ utils/chatgpt.js
@@ -1,5 +1,4 @@
 const { Configuration, OpenAIApi } = require("openai");
-const history = require("./history");
 require("dotenv").config();
 
 const configuration = new Configuration({
@@ -7,19 +6,30 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-module.exports = async (api, message, body) => {
+module.exports = async (message) => {
   try {
-    const recentMsg = await history(api, message);
-    const allMsg = [...recentMsg, {role: "user", content: body}];
-    
+    const messages = [
+      {
+        role: "assistant",
+        content: `You are Xyst Lee, a messenger bot created by Arnel Cutie. You answer as concisely as possible for each responseIf you are generating a list, do not have too many items.
+    Current Date and Time: ${new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Manila",
+    })}\n\n`,
+      },
+      {
+        role: "user",
+        content: message,
+      },
+    ];
+
     const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: allMsg,
-    });
+      model: "gpt-3.5-turbo-0301",
+      messages: messages,
+    }, {timeout: 30000});
     const completion_text = completion.data.choices[0].message.content;
     return { isError: "false", message: completion_text };
   } catch (e) {
     console.log(e)
-    return { isError: "true", message: "Error, salik yawa." };
+    return { isError: "true", message: "Error found!, Lachica sadboi😭" };
   }
 };
